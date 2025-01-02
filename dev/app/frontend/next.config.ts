79634f0d79b58fb6e4d369686743f17d6bb1c176
Tsubasa Moriguchi <wing787tsubasa.m@gmail.com>
2025-01-03 07:25:00 +0900

    completed Enviroment building for Frontend

---
+++ dev/app/frontend/next.config.ts
@@ -0,0 +1,24 @@
+// ↓default
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// 以下は書籍P.90のコード3-2-2をts向けにした
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://host.docker.internal:8000/api/:path*/',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
