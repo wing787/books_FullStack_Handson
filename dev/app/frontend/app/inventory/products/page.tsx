39ddbf2a986ed94f12fc10a9e59936d0195ff230
Tsubasa Moriguchi <wing787tsubasa.m@gmail.com>
2025-01-07 22:17:25 +0900

    make mockup pages

--- dev/app/frontend/app/inventory/products/page.tsx
+++ dev/app/frontend/app/inventory/products/page.tsx
@@ -1,8 +1,87 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import productData from "./sample/dummy_products.json";
+import Link from 'next/link';
+
+type ProductData = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+};
+
+type InputData = {
+    id: string;
+    name: string;
+    price: string;
+    description: string;
+};
+
 export default function Page() {
+    // 読込データを保持
+    const [data, setData] = useState<Array<ProductData>>([]);
+
+    useEffect(() => {
+        setData(productData);
+    }, [])
+
+    // 登録データを保持
+    const [input, setInputData] = useState<InputData>({
+        id: "",
+        name: "",
+        price: "",
+        description: ""
+    });
+    // 登録データの値を更新
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = event.target;
+        setInputData({ ...input, [name]: value });
+    };
+
+    // 新規登録処理、新規登録行の表示状態を保持
+    const [shownNewRow, setShownNewRow] = useState(false);
+    const handleShowNewRow = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(true)
+    };
+    const handleAddCancel = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(false)
+    };
+    const handleAdd = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        // バックエンドを使用した登録処理を呼ぶ
+        setShownNewRow(false)
+    };
+
+    // 更新・削除処理、更新・削除行の表示状態を保持
+    const [editingRow, setEditingRow] = useState(0);
+    const handleEditRow: any = (id: number) => {
+        setShownNewRow(false)
+        setEditingRow(id);
+        const seletedProduct: ProductData = data.find((v) => v.id === id) as ProductData;
+        setInputData({
+            id: id.toString(),
+            name: seletedProduct.name,
+            price: seletedProduct.price.toString(),
+            description: seletedProduct.description,
+        });
+    };
+    const handleEditCancel: any = (id: number) => {
+        setEditingRow(0)
+    };
+    const handleEdit: any = (id: number) => {
+        setEditingRow(0)
+    };
+    const handleDelete: any = (id: number) => {
+        setEditingRow(0)
+    };
+
     return (
         <>
             <h2>商品一覧</h2>
-            <button>商品を追加する</button>
+            <button onClick={ handleShowNewRow }>商品を追加する</button>
             <table>
                 <thead>
                     <tr>
@@ -11,30 +90,41 @@ export default function Page() {
                         <th>単価</th>
                         <th>説明</th>
                         <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>コットン100％バックリボンティアードワンピース（黒）</td>
-                        <td>6900</td>
-                        <td>大人の愛らしさを引き立てる、ナチュラルな風合い。リラックス×トレンドを楽しめる、上品なティアードワンピース。</td>
-                        <td><button>更新・削除</button></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>ライトストレッチカットソー（ネイビー）</td>
-                        <td>2980</td>
-                        <td>しなやかな肌触りが心地よい、程よいフィット感のカットソー。ビジネスカジュアルにも普段使いにも使える、ベーシックなデザイン。</td>
-                        <td><button>更新・削除</button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>ベルト付きデニムパンツ（ブルー）</td>
-                        <td>5980</td>
-                        <td>定番のデニムパンツに、フェミニンなベルトをプラスしたスタイリッシュなアイテム。カジュアルにもきれいめにも合わせやすい。</td>
-                        <td><button>更新・削除</button></td>
-                    </tr>
+                    {shownNewRow ? (
+                        <tr>
+                            <td></td>
+                            <td><input type="text" name="name" onChange={handleInput} /></td>
+                            <td><input type="number" name="price" onChange={handleInput} /></td>
+                            <td><input type="text" name="description" onChange={handleInput} /></td>
+                            <td></td>
+                            <td><button onClick={(event) => handleAddCancel(event)}>キャンセル</button><button onClick={(event) => handleAdd(event)}>登録する</button></td>
+                        </tr>
+                    ): ""}
+                    {data.map((data: any) => (
+                        editingRow === data.id ? (
+                            <tr key={data.id}>
+                                <td>{data.id}</td>
+                                <td><input type="text" value={input.name} name="name" onChange={handleInput} /></td>
+                                <td><input type="number" value={input.price} name="price" onChange={handleInput} /></td>
+                                <td><input type="text" value={input.description} name="description" onChange={handleInput} /></td>
+                                <td></td>
+                                <td><button onClick={() => handleEditCancel(data.id)}>キャンセル</button><button onClick={() => handleEdit(data.id)}>更新する</button><button onClick={() =>handleDelete(data.id)}>削除する</button></td>
+                            </tr>
+                        ) : (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.name}</td>
+                            <td>{data.price}</td>
+                            <td>{data.description}</td>
+                            <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
+                            <td><button onClick={() => handleEditRow(data.id)}>更新・削除</button></td>
+                        </tr>
+                        )
+                    ))}
                 </tbody>
             </table>
         </>
